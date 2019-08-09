cdb48adb8295359f6d63c5c97069fb2e24878074
kevin lang <langkevina@gmail.com>
2019-08-09 18:16:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,15 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
+            item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+            item = @@items.find{|s| s.name == item_name}
+            if item != nil
+              resp.status = 200
+              resp.write item.price
+            else
+              resp.status = 400
+              resp.write "Item not found"
+            end
     else
       resp.write "Route not found"
       resp.status = 404
